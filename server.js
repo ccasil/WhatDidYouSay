516e3c8615abc3601eccc5f277f95d679473d48f
ccasil <cesarkylecasil@gmail.com>
2019-06-24 10:58:52 -0700

    responsiveness

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
 const express = require("express"),
     path = require("path"),
     session = require("express-session"),
-    bodyParser = require('body-parser'),
+    bodyParser = require("body-parser"),
     app = express();
 
 app.use(session({ 
-    secret: 'myname',
+    secret: "myname",
     resave: true,
     saveUninitialized: false
 }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "/public")));
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
 let messages = [];
 let users = [];
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
     console.log("Session ID:", req.sessionID);
     res.render("login");
 })
 
-app.post('/chat', function (req, res) {
+app.post("/chat", function (req, res) {
     var rand1 = Math.floor(Math.random() * 256),
         rand2 = Math.floor(Math.random() * 256),
         rand3 = Math.floor(Math.random() * 256);
     var randcol = "color: rgb(" + rand1 + ", " + rand2 + "," + rand3 + ");";
     req.session.color = randcol;
     req.session.body = req.body;
-    console.log(req.session.color, "Username: " + req.session.body.username);
+    console.log("Username: " + req.session.body.username, req.session.color);
     res.redirect("/chat");
 })
 
-app.get('/chat', function (req, res) {
+app.get("/chat", function (req, res) {
     if(req.session.body == undefined){
         res.render("login");
     }
@@ -46,15 +46,15 @@ const server = app.listen(8000, function () {
 });
 
 // socket.io
-const io = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
+const io = require("socket.io").listen(server);
+io.sockets.on("connection", function (socket) {
 
     users.push(socket.id);
     console.log("Socket ID:", socket.id);
     console.log("Current Users: ", users);
 
-    socket.on('disconnect', function () {
-        console.log('User Disconnected:', socket.id);
+    socket.on("disconnect", function () {
+        console.log("User Disconnected:", socket.id);
         for (var i = 0; i < users.length; i++) {
             if (users[i] == socket.id) {
                 users.splice(i, 1);
@@ -74,7 +74,7 @@ io.sockets.on('connection', function (socket) {
             color: c
         });
 
-        io.emit('updatedmessage', {
+        io.emit("updatedmessage", {
             name: data.name,
             message: data.message,
             color: data.color,
